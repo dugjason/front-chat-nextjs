42399128e5a5d232dfded4c7b47354e4fd018f7e
Jason Dugdale <jason.dugdale@gmail.com>
2024-05-10 12:51:59 -0700

    Install Front Chat

---
+++ components/front-chat.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Script from "next/script";
+
+export function FrontChat() {
+  return (
+    <Script
+      id="front-chat-script"
+      src="https://chat-assets.frontapp.com/v1/chat.bundle.js"
+      onLoad={initFrontChat}
+    ></Script>
+  );
+}
+
+function initFrontChat() {
+  //@ts-expect-error
+  window.FrontChat("init", {
+    chatId: "3c52b76a98df2d446d85d73bebdd7f5d",
+    useDefaultLauncher: true,
+  });
+}
